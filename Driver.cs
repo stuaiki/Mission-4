5a974025f37f4112adb3e6cb250b7bb3a041acbc
stuaiki <takaku@byu.edu>
2024-01-26 16:38:21 -0700

    Add project files.

---
+++ Driver.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    static char[,] board = new char[3, 3];
+    private static void Main(string[] args)
+    {
+        // Welcome the user to the game
+        Console.WriteLine("Welcome to the Tic-Tac-Toe game!");
+
+        Console.WriteLine(" 1 2 3");
+        for (int row = 0; row < 3; row++)
+        {
+            Console.Write(row + 1);
+            for (int col = 0; col < 3; col++)
+            {
+                Console.Write($" {board[row, col]}");
+                if (col < 2) Console.Write("|");
+            }
+            Console.WriteLine();
+            if (row < 2) Console.WriteLine(" -----");
+        }
+    }
+
+
+}
