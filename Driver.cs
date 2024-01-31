8618b988f80c01f6b2547f53a9ec41d8e8f6168c
stuaiki <takaku@byu.edu>
2024-01-31 16:22:00 -0700

    new commit

--- Driver.cs
+++ Driver.cs
@@ -12,3 +12,11 @@ rowNum = Console.ReadLine();
 
 Console.WriteLine("Which column do you want to put? (1~3) ");
 colNum = Console.ReadLine();
+
+if (rowNum == "1")
+{
+    if (colNum == "1")
+    {
+        boardArray[0].add('ss')
+    }
+}
